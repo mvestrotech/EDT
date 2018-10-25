cda18c1622d20afc38ca434f70ebee5526f75915
DarkJP <darkjeanpaul@free.Fr>
2018-10-25 18:53:29 +0200

    bllallfsf

--- controller/ProfController.php
+++ controller/ProfController.php
@@ -5,8 +5,25 @@ class ProfController{
 
   //Retourne tous les profs
   public function index(){
-    $periodes = Periode::getAllProfs();
-    require('vue/layout/edt.html');
+    $profs = Periode::getAllProfs();
+    $array_json = array();
+
+    while ($prof = $profs->fetch()){
+      $obj_json = array('id' => utf8_encode($prof['id_prof']),
+      'nom' => utf8_encode($prof['nom']),
+      'prenom' => utf8_encode($prof['prenom']), 
+      'email' => utf8_encode($prof['email']),
+      'label' => utf8_encode($prof['label']),
+      'login' => utf8_encode($prof['login_prof']),
+      'pass' => utf8_encode($prof['pass_prof']),
+      'date' => utf8_encode($prof['date_prof']),
+      'photo' => utf8_encode($prof['urlPhoto']),
+      'couleur' => utf8_encode($prof['couleur']),
+      'bConnect' => utf8_encode($prof['bConnect']),
+    );
+      array_push($array_json,$obj_json);
+    }
+    echo  json_encode($array_json);
   }
 }
 ?>
