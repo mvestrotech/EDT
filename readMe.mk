65ce39d99db3e44519de6015812c38618321a2dc
Nicolas Garnier <nicolasgarnier97@gmail.com>
2018-10-28 22:06:39 +0100

    Update and rename readMe.txt to readMe.mk

---
+++ readMe.mk
@@ -0,0 +1,8 @@
+# Connexion à la base de données
+
+Afin que l'application fonctionne correctement le fichier `model/connectDb.php` doit renseigner les configurations de votre serveur **MySQL**.
+
+
+# Mise en place de la base de données
+
+Pour installer la base de données utilisé pour l'application un script est présent afin qu'il soit installer dans une table nommé `proj` créée par vos soins. le script est situé dans le dossier `resources` sous le nom de `proj.sql`.
